f66e355c5ee1407015b47b330bb11560d1e5af44
Vítor Barroso <contact@owozsh.dev>
2024-01-13 21:56:06 -0300

    Add expo font and zustand

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, TamaguiProvider, Text, YStack } from "tamagui";
+import tamaguiConfig from "./tamagui.config";
+import { useFonts } from "expo-font";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>heheehhehe</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
-}
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+  return loaded ? (
+    <TamaguiProvider config={tamaguiConfig}>
+      <StatusBar style="auto" />
+      <YStack fullscreen justifyContent="flex-end" padding="$4" gap="$4">
+        <YStack justifyContent="center" alignItems="center" flex={1}>
+          <Text fontSize="$9">mwro</Text>
+        </YStack>
+        <Button backgroundColor="#000" color="#FFF">
+          Acessar Conta
+        </Button>
+        <Button>Criar Conta</Button>
+      </YStack>
+    </TamaguiProvider>
+  ) : null;
+}
