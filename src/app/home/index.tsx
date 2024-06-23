6e3d6739cc611db6a2fc9d24a697c13dd7ac3a13
Vítor Barroso <contact@owozsh.dev>
2024-06-23 11:07:29 -0300

    Prettier

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,33 +1,33 @@
-import { useRouter } from "expo-router";
-import useAuth from "../../hooks/useAuth";
-import Button from "../../ui/Button";
-import Text from "../../ui/Text";
-import VStack from "../../ui/VStack";
+import useAuth from '@hooks/useAuth'
+import Button from '@ui/Button'
+import Text from '@ui/Text'
+import VStack from '@ui/VStack'
+import { useRouter } from 'expo-router'
 
 export default function Home() {
-  const router = useRouter();
-  const { sign_out } = useAuth();
+  const router = useRouter()
+  const { sign_out } = useAuth()
 
   return (
-    <VStack flex={1} items="center" justify="center" gap={20}>
-      <Text size={28} weight="600">
+    <VStack flex={1} items='center' justify='center' gap={20}>
+      <Text size={28} weight='600'>
         Home
       </Text>
       <Button
-        variant="primary"
-        onPress={() => router.push("/community/create/")}
+        variant='primary'
+        onPress={() => router.push('/community/create/')}
       >
         Criar comunidade
       </Button>
       <Button
-        variant="primary"
-        onPress={() => router.push("/community/edit/5")} // REPLACE WITH COMMUNITY ID
+        variant='primary'
+        onPress={() => router.push('/community/edit/5')}
       >
         Editar comunidade
-      </Button> 
-      <Button variant="primary" onPress={sign_out}>
+      </Button>
+      <Button variant='primary' onPress={sign_out}>
         Sair
       </Button>
     </VStack>
-  );
+  )
 }
