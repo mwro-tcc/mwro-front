ff8cccb075451a449399e431db557db22b980f9a
Maria Isabel <moliveira23@uolinc.com>
2024-08-17 17:47:59 -0300

    feat: leave community

--- src/forms/Store/components/StoreFormStep1.tsx
+++ src/forms/Store/components/StoreFormStep1.tsx
@@ -1,15 +1,13 @@
 import { StoreForm } from '@src/types/store'
-import Switch from '@ui/Switch'
 import TextInput from '@ui/TextInput'
 import { UseFormReturn } from 'react-hook-form'
 
 type Props = {
   form: UseFormReturn<StoreForm, any, StoreForm>
-  storeCommunity: any
 }
 
 export default function StoreFormStep1(props: Props) {
-  const { form, storeCommunity } = props
+  const { form } = props
 
   return (
     <>
