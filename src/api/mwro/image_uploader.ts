421f5c1445ebfff8e0c3ec05d947d55ab3d225d3
Vítor Barroso <contact@owozsh.dev>
2024-10-20 16:18:08 -0300

    Update image picker

--- src/api/mwro/image_uploader.ts
+++ src/api/mwro/image_uploader.ts
@@ -1,7 +1,7 @@
 import createConsoleErrorHandler from "@lib/create_console_error_handler";
 import Api from "./api";
 import Routes from "./routes";
-import { deeplog } from "@lib/deeplog";
+import { ImagePickerAsset } from "expo-image-picker";
 
 enum ImageUploaderError {
   LOAD_FILE = 'Erro ao carregar a imagem para realizar o upload',
@@ -9,48 +9,20 @@ enum ImageUploaderError {
   API = 'Erro na requisição'
 }
 
-const IMAGE_UPLOADER_CONFIG = {
-  headers: {
-    'Content-Type': `multipart/form-data`,
-  }
-}
-
-function createFormData(blob: Blob, fileName: string) {
-  let form = new FormData();
-  form.append('image', new File([blob], fileName), fileName);
-  return form
-}
-
-function createFileName(assetId: string, localSource: string) {
-  const fileExtension = localSource.split('.').pop()
-  return `${assetId}.${fileExtension}`
-}
-
 class ImageUploader {
-  static async upload(assetId: string, localSource: string) {
-    const file =
-      await fetch(localSource)
-        .catch(createConsoleErrorHandler(ImageUploaderError.LOAD_FILE))
+  static async upload(assetId: string, image: ImagePickerAsset) {
+    const form = new FormData();
+    const fileExtension = image.uri.split('.').pop()
+    const data = { uri: image.uri, name: 'media', type: `image/${fileExtension}` } as any
+    form.append('image', data);
 
-    if (!file) return
-
-    const blob =
-      await file.blob()
-        .catch(createConsoleErrorHandler(ImageUploaderError.LOAD_BLOB))
-
-    if (!blob) return
-
-    const form =
-      createFormData(blob, createFileName(assetId, localSource))
-
-    Api.post(
+    return Api.post(
       Routes.Image.create(assetId),
       form,
-      IMAGE_UPLOADER_CONFIG
     ).catch(createConsoleErrorHandler(ImageUploaderError.API))
   }
   static createUploader(assetId: string) {
-    return (localSource: string) => ImageUploader.upload(assetId, localSource)
+    return (image: ImagePickerAsset) => ImageUploader.upload(assetId, image)
   }
 }
 
