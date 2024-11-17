84fc932e271de47505fa3b51a6adaac69c36e0a6
Maria Isabel <moliveira23@uolinc.com>
2024-11-17 01:36:07 -0300

    fix: community link

--- src/app/main/(tabs)/(account,explore,favorites)/communities/[id]/index.tsx
+++ src/app/main/(tabs)/(account,explore,favorites)/communities/[id]/index.tsx
@@ -74,7 +74,7 @@ export default function Community() {
     }
   }
 
-  const communityLink = createURL(`/communities/${id}`)
+  const communityLink = createURL(`/main/(explore)/communities/${id}`)
 
   const copyToClipboard = async () => {
     await Clipboard.setStringAsync(communityLink)
